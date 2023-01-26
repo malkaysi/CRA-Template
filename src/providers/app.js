da61e4b1f5593995f4907806eceb390a1c97b56c
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2023-01-26 15:26:00 -0500

    Adding 404 Page

--- src/providers/app.js
+++ src/providers/app.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { RouterProvider } from "react-router-dom";
+import { protectedRoutes } from "../routes/protected";
 
-import { BrowserRouter as Router } from "react-router-dom";
-
-export const AppProvider = ({ children }) => {
-  return <Router>{children}</Router>;
+export const AppProvider = () => {
+  return <RouterProvider router={protectedRoutes} />;
 };
 
 AppProvider.propTypes = {
--- src/routes/protected.js
+++ src/routes/protected.js
@@ -1,6 +1,7 @@
-import { Outlet } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 import { MainLayout } from "../components/Layout/MainLayout";
 import { Dashboard } from "../features/dashboard/routes";
+import { NotFound } from "../features/misc/routes";
 import { Profile } from "../features/profile/routes";
 
 const App = () => {
@@ -11,13 +12,14 @@ const App = () => {
   );
 };
 
-export const protectedRoutes = [
+export const protectedRoutes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <NotFound />,
     children: [
-      { path: "/", element: <Dashboard /> },
+      { path: "/dashboard", element: <Dashboard /> },
       { path: "/profile", element: <Profile /> },
     ],
   },
-];
+]);
